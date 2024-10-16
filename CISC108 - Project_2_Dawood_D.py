401328c688d46b515930e18dd2ed5bf8483fb51b
Dina Dawood <ddawood@udel.edu>
2024-10-16 12:55:58 -0400

    Add files via upload

---
+++ CISC108 - Project_2_Dawood_D.py
@@ -0,0 +1,182 @@
+# Part 1) Set-up
+# Load turtle module, move it to topleft corner, make turtle faster
+import turtle
+turtle.reset()
+turtle.speed(10)
+turtle.penup()
+turtle.goto(-450, 300)
+turtle.pendown()
+turtle.speed(10)
+
+# Part 2) Drawing
+# Write the code for each letter below
+
+# First letter
+# "H"
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(30)
+turtle.backward(60)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(30)
+turtle.backward(60)
+
+turtle.penup()
+turtle.goto(-410, 300)
+turtle.pendown()
+turtle.speed(10)
+# Second letter
+# "E"
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(25)
+turtle.backward(25)
+turtle.left(90)
+turtle.backward(30)
+turtle.left(90)
+turtle.backward(20)
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(25)
+
+turtle.penup()
+turtle.goto(-380, 330)
+turtle.pendown()
+turtle.speed(10)
+
+# Third letter
+# "L"
+turtle.right(90)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(25)
+
+turtle.penup()
+turtle.goto(-350, 330)
+turtle.pendown()
+turtle.speed(10)
+
+# Fourth letter
+# "L"
+turtle.right(90)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(25)
+
+turtle.penup()
+turtle.goto(-300, 272)
+turtle.pendown()
+turtle.speed(10)
+
+# Fifth letter
+# "O"
+turtle.circle(30)
+
+turtle.penup()
+turtle.goto(-240, 330)
+turtle.pendown()
+turtle.speed(10)
+
+# Sixth letter
+# "D"
+turtle.right(90)
+turtle.forward(62)
+turtle.left(90)
+turtle.circle(70,30)
+turtle.left(20)
+turtle.forward(5)
+turtle.circle(30, 170)
+
+turtle.penup()
+turtle.goto(-190, 330)
+turtle.pendown()
+turtle.speed(10)
+
+# Seventh letter
+# "D"
+turtle.left(50)
+turtle.forward(60)
+turtle.left(90)
+turtle.circle(70,30)
+turtle.left(20)
+turtle.forward(5)
+turtle.circle(30, 170)
+
+turtle.penup()
+turtle.goto(-130, 325)
+turtle.pendown()
+turtle.speed(10)
+
+# Eighth letter
+# "A"
+turtle.left(40)
+turtle.forward(30)
+turtle.left(100)
+turtle.forward(25)
+turtle.backward(25)
+turtle.right(100)
+turtle.forward(30)
+turtle.backward(65)
+turtle.left(100)
+turtle.forward(15)
+turtle.right(80)
+turtle.forward(65)
+
+turtle.penup()
+turtle.goto(-105, 330)
+turtle.pendown()
+turtle.speed(10)
+
+# Nineth letter
+# "W"
+turtle.left(10)
+turtle.forward(65)
+turtle.left(140)
+turtle.forward(60)
+turtle.right(140)
+turtle.forward(60)
+turtle.left(140)
+turtle.forward(65)
+
+turtle.penup()
+turtle.goto(40, 290)
+turtle.pendown()
+turtle.speed(10)
+
+# Tenth letter
+# "O"
+turtle.circle(30)
+
+turtle.penup()
+turtle.goto(105, 290)
+turtle.pendown()
+turtle.speed(10)
+
+# Eleventh letter
+# "O"
+turtle.circle(30)
+
+turtle.penup()
+turtle.goto(115, 330)
+turtle.pendown()
+turtle.speed(10)
+
+# Twelevth letter
+# "D"
+turtle.right(160)
+turtle.forward(60)
+turtle.left(90)
+turtle.circle(70,30)
+turtle.left(20)
+turtle.forward(5)
+turtle.circle(30, 170)
+
+turtle.penup()
+turtle.goto(150, 270)
+turtle.pendown()
+turtle.speed(10)
